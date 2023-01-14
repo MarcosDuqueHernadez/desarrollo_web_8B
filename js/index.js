0774f7def5f89a27aa2a92522689e2ff0e3e205e
Marcos Duque <duquehem@gmail.com>
2023-01-14 02:10:22 -0600

    cambios index

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function mensaje(){
+    console.log("hola esto es un mensaje solamente");
+}
+
+
+
+
+function getCategorias() {
+    fetch('https://api.escuelajs.co/api/v1/categories').then(function(response){
+        console.log("si pasoo padrine");
+    })
+}
+
+
+/* https://api.lorem.space/image/fashion?w=640&h=480&r=6181 */
+/* https://box-shadow.dev/ */
